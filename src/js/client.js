9c9a26f83a7335549b4bd23009a34117a4dd684b
dudueasy <dudueasy@126.com>
2019-03-17 18:24:31 +0800

    example for Redux function combineReducers

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,46 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Layout from "./components/Layout";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Layout from "./components/Layout";
+//
+// const app = document.getElementById("root");
+//
+// ReactDOM.render(<Layout />, app);
 
-const app = document.getElementById("root");
+import {combineReducers, createStore} from 'redux';
 
-ReactDOM.render(<Layout />, app);
+// receive state.user as it's state argument
+const userReducer = (state = {}, action) => {
+  switch (action.type) {
+    case "CHANGE_NAME":
+      state = {...state, name: action.payload};
+      break;
+
+    case "CHANGE_AGE":
+      state = {...state, age: action.payload};
+      break;
+  }
+
+  return state;
+};
+
+// receive state.tweet as it's state argument
+const tweetReducer = (state = [], action) => {
+  return state;
+};
+
+
+const reducers = combineReducers({
+  user: userReducer,
+  tweet: tweetReducer,
+});
+
+
+const store = createStore(reducers);
+
+store.subscribe(() => {
+  console.log("store changed to be: ", store.getState());
+});
+
+store.dispatch({type: "CHANGE_NAME", payload: "Will"});
+store.dispatch({type: "CHANGE_AGE", payload: "20"});
+store.dispatch({type: "CHANGE_AGE", payload: "21"});
